b2c414d60546ff972807296ecab2bc4e6551f75f
Dhanraj H Kutwade <110124614+dhanrajreddy@users.noreply.github.com>
2023-11-29 00:34:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
-let words = touristSpots.join(" "); // Used join to create a string from the array
-let articlelessWords = words.split(" ").filter(word => {
-    return !(word === 'a' || word === 'an' || word === 'the' ||
-    word === 'A' || word === 'An' || word === 'The');
-});
+function sortMethod(words){
+	let articlelessWords = words.split(" ").filter(word => {
+	    return !(word === 'a' || word === 'an' || word === 'the' ||
+	    word === 'A' || word === 'An' || word === 'The');
+	});
+	return articlelessWords.join(" ")
 
-articlelessWords.sort();
+}
 
-console.log(articlelessWords);
+let hashMap={};
+let arr=[];
+for(let i=0;i<touristSpots.length;i++){
+	let key=sortMethod(touristSpots[i]);
+    hashMap[key]=touristSpots[i];
+	arr.push(key);
+}
+arr.sort();
+let outputList=[];
+arr.forEach((output)=>{
+    outputList.push(hashMap[output])
+})
+                    
+    console.log(outputList)
