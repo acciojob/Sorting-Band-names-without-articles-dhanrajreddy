2836dc2214bf92464e94597769cc4aa76d7d380c
Dhanraj H Kutwade <110124614+dhanrajreddy@users.noreply.github.com>
2023-11-29 00:41:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
-let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
-function sortMethod(words){
-	let articlelessWords = words.split(" ").filter(word => {
-	    return !(word === 'a' || word === 'an' || word === 'the' ||
-	    word === 'A' || word === 'An' || word === 'The');
-	});
-	return articlelessWords.join(" ")
+let bandNames = ['The Rolling Stones', 'Led Zeppelin', 'The Beatles', 'Queen', 'Pink Floyd'];
 
+// Function to remove articles from band names
+function removeArticles(name) {
+    // Regular expression to match articles at the beginning of a band name
+    const articleRegex = /^(a|an|the)\s/i;
+    // Remove articles and return the modified name
+    return name.replace(articleRegex, '');
 }
 
-let hashMap={};
-let arr=[];
-for(let i=0;i<touristSpots.length;i++){
-	let key=sortMethod(touristSpots[i]);
-    hashMap[key]=touristSpots[i];
-	arr.push(key);
-}
-arr.sort();
-let outputList=[];
-arr.forEach((output)=>{
-    outputList.push(hashMap[output])
-})
-                    
-    console.log(outputList)
+// Sorting the band names in lexicographic order without articles
+bandNames.sort((a, b) => removeArticles(a).localeCompare(removeArticles(b)));
+
+// Output the sorted band names inside ul id='band' tag using li tag
+document.getElementById('band').innerHTML = bandNames.map(name => `<li>${name}</li>`).join('');
