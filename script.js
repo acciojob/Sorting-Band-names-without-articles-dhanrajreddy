b9cf2f01602365a522eda8b62a9274d34e8e6508
Dhanraj H Kutwade <110124614+dhanrajreddy@users.noreply.github.com>
2023-11-29 00:45:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-let bandNames = ['The Rolling Stones', 'Led Zeppelin', 'The Beatles', 'Queen', 'Pink Floyd'];
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
-// Function to remove articles from band names
 function removeArticles(name) {
-    // Regular expression to match articles at the beginning of a band name
-    const articleRegex = /^(a|an|the)\s/i;
-    // Remove articles and return the modified name
-    return name.replace(articleRegex, '');
+    const articles = ['a', 'an', 'the'];
+    const words = name.split(' ');
+
+    // Filter out articles from the name
+    const filteredWords = words.filter(word => !articles.includes(word.toLowerCase()));
+    return filteredWords.join(' ');
 }
 
-// Sorting the band names in lexicographic order without articles
-bandNames.sort((a, b) => removeArticles(a).localeCompare(removeArticles(b)));
+let hashMap = {};
+let arr = [];
 
-// Output the sorted band names inside ul id='band' tag using li tag
-document.getElementById('band').innerHTML = bandNames.map(name => `<li>${name}</li>`).join('');
+for (let i = 0; i < touristSpots.length; i++) {
+    let key = removeArticles(touristSpots[i]);
+    hashMap[key] = touristSpots[i];
+    arr.push(key);
+}
+
+// Sort the keys without articles
+arr.sort();
+
+let outputList = [];
+arr.forEach(output => {
+    outputList.push(hashMap[output]);
+});
+
+console.log(outputList);
