99c241718d843d18f01b9f752d372e0f8b2cf5cb
Dhanraj H Kutwade <110124614+dhanrajreddy@users.noreply.github.com>
2023-11-28 12:22:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 //your code here
-
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let words= touristSpots.split(" ");
+	words.filter((word)=>{
+		let smallCaseWord=word.toLowerCase(word);
+		if(smallCaseWord===a || smallCaseWord===an || smallCaseWord === the)
+				return false;
+		else
+			return true;
+	}
+);
