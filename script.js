fc1fbcc4021496e0aab2bdce3ecafd2d956640af
Dhanraj H Kutwade <110124614+dhanrajreddy@users.noreply.github.com>
2023-11-29 22:07:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
 let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+function sortMethod(words){
+	let articlelessWords = words.toLowerCase().split(" ").filter(word => {
+	   if(word === 'a' || word === 'an' || word === 'the')
+			return false;
+		else
+			return true;
+	});
+	return articlelessWords.join(" ")
 
-function removeArticles(name) {
-    const articles = ['a', 'an', 'the'];
-    const words = name.split(' ');
-
-    // Filter out articles from the name
-    const filteredWords = words.filter(word => !articles.includes(word.toLowerCase()));
-    return filteredWords.join(' ');
 }
 
-let hashMap = {};
-let arr = [];
-
-for (let i = 0; i < touristSpots.length; i++) {
-    let key = removeArticles(touristSpots[i]);
-    hashMap[key] = touristSpots[i];
-    arr.push(key);
+let hashMap={};
+let arr=[];
+for(let i=0;i<touristSpots.length;i++){
+	let key=sortMethod(touristSpots[i]);
+    hashMap[key]=touristSpots[i];
+	arr.push(key);
 }
-
-// Sort the keys without articles
-arr.sort();
-
-let outputList = [];
-arr.forEach(output => {
-    outputList.push(hashMap[output]);
-});
-
+ arr.sort();
+let outputList=[];
+arr.forEach((output)=>{
+    outputList.push(hashMap[output])
+})
+                    
 console.log(outputList);
